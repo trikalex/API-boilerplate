be2198f14151e427f2dfa5f0798911e7c733c4b7
Alexandru Ursu <bodhipankh@yahoo.com>
2023-05-01 01:32:44 +0000

    Adding function for api key status display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-BgOA2rq2zYGcq7WwFkREHjpzg-o
+const API_KEY = "BgOA2rq2zYGcq7WwFkREHjpzg-o";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
+    }
+};
+
+function displayStatus(data) {
+
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerHTML = heading;
+    document.getElementById("results-content").innerHTML = results;
+    
+    resultsModal.show();
+}
