48851fa3f3e7415aa9a22782e969cf7ea5c4cd76
Alexandru Ursu <bodhipankh@yahoo.com>
2023-05-03 02:16:19 +0000

    Add post request code in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,19 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        }, 
+        body: form,
+    })
+}
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -26,6 +39,6 @@ function displayStatus(data) {
 
     document.getElementById("resultsModalTitle").innerHTML = heading;
     document.getElementById("results-content").innerHTML = results;
-    
+
     resultsModal.show();
 }
